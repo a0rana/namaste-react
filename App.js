f16252f4f2669983c69077b3f21f7ebb2ca1628d
Anurag Rana <anurag.rana@oracle.com>
2025-03-17 09:48:28 +0530

    ignited project

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+
 const header = React.createElement('div', { id: 'parent' },
     React.createElement('div', { id: 'child' },
         [React.createElement('h1', {}, 'inside header'),
         React.createElement('h2', {}, 'inside header 2')]));
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 root.render(header);
